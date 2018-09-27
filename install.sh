ab37a164be62653d976930be35d174f9cb7885bc
Christoph Dorn <christoph@christophdorn.com>
2018-09-26 17:17:12 -0700

    misc

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 sourcePath="$(node --eval 'process.stdout.write(require.resolve("pinf-loader-js/loader.js"));')"
 
-ln -s "${sourcePath}" "scripts/pinf-loader.js"
+if [ ! -e "scripts/pinf-loader.js" ]; then
+    ln -s "${sourcePath}" "scripts/pinf-loader.js"
+fi
