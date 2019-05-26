8de204302a460f52d73ad61661d24f1f6c6ace25
Christoph Dorn <christoph@christophdorn.com>
2019-05-25 22:05:12 -0700

    regenerator runtime

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash.origin.script
 
+sourcePath="$(node --eval 'process.stdout.write(require.resolve("pinf-loader-js/dist/babel-regenerator-runtime.js"));')"
+cp "${sourcePath}" "scripts/lib/babel-regenerator-runtime.js"
+
 sourcePath="$(node --eval 'process.stdout.write(require.resolve("pinf-loader-js/dist/loader-core.browser.js"));')"
 cp "${sourcePath}" "scripts/lib/pinf-loader-core.browser.js"
 cp "scripts/lib/pinf-loader-core.browser.js" "examples/01-HelloWorld/extension/scripts/lib/pinf-loader-core.browser.js"
